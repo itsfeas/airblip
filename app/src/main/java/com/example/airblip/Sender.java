52c5b4568f4cb60ac9ff3207c54bac61dc35eee1
itsfeas <amf.fiaz@gmail.com>
2022-01-16 01:12:19 -0700

    fixed broken sender connections

--- app/src/main/java/com/example/airblip/Sender.java
+++ app/src/main/java/com/example/airblip/Sender.java
@@ -1,31 +1,12 @@
 package com.example.airblip;
 
-import android.app.Service;
-import android.content.Intent;
-import android.media.AudioAttributes;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.IBinder;
-import android.os.Message;
-import android.os.Messenger;
-import android.os.RemoteException;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 
 import com.google.common.primitives.Bytes;
 
-
-
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 
@@ -35,7 +16,7 @@ public class Sender {
     private AudioTrack initBlip;
     private AudioTrack dataBlip;
 
-    private double transferFreq = 100;                                  // hz
+    private double transferFreq = 50;                                  // hz
     private double soundFreq = 1500;                                    // hz
     private int sampleRate = 20000;                                     // hz
 
@@ -62,6 +43,10 @@ public class Sender {
         setSendBytes(bytes);
     }
 
+    private byte[] getDataBytes() {
+        return Bytes.toArray(this.file);
+    }
+
     public void setSendBytes(List<Byte> bytes) {
         this.file = bytes;
     }
@@ -85,7 +70,8 @@ public class Sender {
                     this.initNumBytes,
                     AudioTrack.MODE_STATIC
             );
-            dataBlip.write((ByteBuffer) this.file, 0, this.file.size());
+            byte[] bytes = getDataBytes();
+            dataBlip.write(bytes, 0, this.file.size());
             this.dataBlip = dataBlip;
         } catch (IOException e) {
             e.printStackTrace();
